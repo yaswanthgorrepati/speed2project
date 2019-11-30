85df6582c5d8a08f99b3fa39ad24caec3dbc9f7a
yaswanthgorrepati <gorrepatiyaswanth2@gmail.com>
2019-11-30 09:11:26 +0530

    project initialization

---
+++ speed2project/src/main/java/com/codesimple/speed2project/Speed2projectApplication.java
@@ -0,0 +1,13 @@
+package com.codesimple.speed2project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Speed2projectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Speed2projectApplication.class, args);
+	}
+
+}
---
+++ speed2project/src/test/java/com/codesimple/speed2project/Speed2projectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codesimple.speed2project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Speed2projectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
